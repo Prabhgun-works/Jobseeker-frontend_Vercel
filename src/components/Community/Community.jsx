39eae7c55b7a67419cf924fdbb6f6205d38c876d
Prabhgun Singh <godsent@Prabhguns-MacBook-Air.local>
2025-07-11 04:34:47 +0530

    Courses fix

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import { useState } from "react"
-import communityData from "../../data/Community.json"
-import "./Community.css"
+import communityData from "../../data/community.json";
+import "./Community.css";
 
 const JoinCommunity = () => {
   const [activeTab, setActiveTab] = useState("popular")
