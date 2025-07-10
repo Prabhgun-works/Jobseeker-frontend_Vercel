a86b663484650147cb1215ca5d2c497c0775345b
Prabhgun Singh <godsent@Prabhguns-MacBook-Air.local>
2025-07-11 04:36:51 +0530

    Fix2 Community

--- src/components/Community/CommunityDialog.jsx
+++ src/components/Community/CommunityDialog.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { useNavigate } from "react-router-dom"
-import communityData from "../../data/Community.json"
+import communityData from "../../data/community.json"
 import "./Community.css"
 
        {/* useNavigate : Dynamic navigation based on data: */}
